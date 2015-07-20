39552c0600fb2e2521e684b5058d0bb09e80d54c
Sebastian Himberger <sebastian@himberger.de>
2015-07-20 22:49:28 +0200

    Made debug into a flag

--- debug.go
+++ debug.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 )
+
+var enableDebugging = false
+
+func init() {
+	flag.BoolVar(&enableDebugging, "debug", false, "debug output")
+}
 
 var debug Debugger = false
 
 type Debugger bool
 
 func (d Debugger) Printf(format string, args ...interface{}) {
-	if d {
+	if enableDebugging {
 		log.Printf("DEBUG: "+format, args...)
 	}
 }
