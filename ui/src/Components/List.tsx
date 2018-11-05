e6d7f74b727658aab3a7e4bc80cfb18f8d15bd1e
Sebastian Himberger <sebastian@himberger.de>
2018-11-05 09:02:00 +0100

    Added search field

--- ui/src/Components/List.tsx
+++ ui/src/Components/List.tsx
@@ -1,29 +1,75 @@
 import * as React from 'react';
-import { Link } from "react-router-dom";
-
 import AppBar from '@material-ui/core/AppBar';
+import BackIcon from '@material-ui/icons/ChevronLeft';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import classNames from 'classnames';
+import Folder from './Folder';
+import getParent from '../getParent';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
-import { withStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import ListMessage from './ListMessage';
+import SearchIcon from '@material-ui/icons/Search';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import BackIcon from '@material-ui/icons/ChevronLeft';
-import classNames from 'classnames';
+import Video from './Video';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 
-import getParent from '../getParent'
-import Folder from './Folder'
-import ListMessage from './ListMessage';
-import Video from './Video'
 
-const styles = {
+const styles = theme => ({
 	root: {
 		flexGrow: 1,
 	},
+	search: {
+		position: 'relative' as 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25),
+		},
+		marginRight: theme.spacing.unit * 2,
+		marginLeft: 0,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			marginLeft: theme.spacing.unit * 3,
+			width: 'auto',
+		},
+	},
+	searchIcon: {
+		width: theme.spacing.unit * 9,
+		height: '100%',
+		position: 'absolute' as 'absolute',
+		pointerEvents: 'none' as 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	grow: {
+		flexGrow: 1,
+	},
+	inputRoot: {
+		color: 'inherit',
+		width: '100%',
+	},
+	inputInput: {
+		paddingTop: theme.spacing.unit,
+		paddingRight: theme.spacing.unit,
+		paddingBottom: theme.spacing.unit,
+		paddingLeft: theme.spacing.unit * 10,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('md')]: {
+			width: 200,
+		},
+	},
 	title: {
-		marginLeft: '24px'
+		marginLeft: '24px',
+		overflow: 'hidden',
+		whiteSpace: 'nowrap' as 'nowrap',
 	}
-};
+});
 
 class List extends React.Component<any, any> {
 
@@ -31,7 +77,8 @@ class List extends React.Component<any, any> {
 		super(props)
 		this.state = {
 			'folders': null,
-			'videos': null
+			'videos': null,
+			'search': ''
 		}
 	}
 
@@ -40,6 +87,7 @@ class List extends React.Component<any, any> {
 		this.setState({
 			'folders': null,
 			'name': null,
+			'search': '',
 			'path': null,
 			'videos': null
 		})
@@ -59,6 +107,19 @@ class List extends React.Component<any, any> {
 		});
 	}
 
+	public filter(items) {
+		if (this.state.search) {
+			return items.filter(item => item.name.toLowerCase().indexOf(this.state.search.toLowerCase()) >= 0)
+		}
+		return items
+	}
+
+	onSearch = (e) => {
+		this.setState({
+			search: e.target.value
+		})
+	}
+
 	public componentDidMount() {
 		const path = this.splat().path || "";
 		this.fetchData(path)
@@ -76,10 +137,10 @@ class List extends React.Component<any, any> {
 		let folders = []
 		let videos = []
 		if (this.state.folders) {
-			folders = this.state.folders.map((folder) => <Grid key={folder.name} item={true} xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}><Folder name={folder.name} path={folder.path} /></Grid>)
-			videos = this.state.videos.map((video) => <Grid key={video.name} item={true} xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}><Video name={video.name} info={video.info} path={video.path} /></Grid>)
+			folders = this.filter(this.state.folders).map((folder) => <Grid key={folder.name} item={true} xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}><Folder name={folder.name} path={folder.path} /></Grid>)
+			videos = this.filter(this.state.videos).map((video) => <Grid key={video.name} item={true} xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}><Video name={video.name} info={video.info} path={video.path} /></Grid>)
 		}
-		const empty = (this.state.folders != null && (videos.length + folders.length) === 0) ? <ListMessage>No folders or videos found in folder</ListMessage> : null
+		const empty = (this.state.folders != null && (videos.length + folders.length) === 0) ? <ListMessage>No folders or videos found</ListMessage> : null
 		return (
 			<div className="list">
 
@@ -93,6 +154,21 @@ class List extends React.Component<any, any> {
 						<Typography variant="h6" className={classNames(classes.title)} color="inherit" >
 							{this.splat().name}
 						</Typography>
+						<div className={classes.grow} />
+						<div className={classes.search}>
+							<div className={classes.searchIcon}>
+								<SearchIcon />
+							</div>
+							<InputBase
+								value={this.state.search}
+								placeholder="Search…"
+								onChange={this.onSearch}
+								classes={{
+									root: classes.inputRoot,
+									input: classes.inputInput,
+								}}
+							/>
+						</div>
 					</Toolbar>
 				</AppBar>
 				<div style={{ padding: 20, paddingTop: '84px' }}>
