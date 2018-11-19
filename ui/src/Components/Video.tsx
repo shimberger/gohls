cad6b3fce051abcd4dbbfd6c7bd3e5153ad9b80d
alai04 <alai04@gmail.com>
2018-11-19 22:20:15 +0800

    add feature of partial download

--- ui/src/Components/Video.tsx
+++ ui/src/Components/Video.tsx
@@ -2,7 +2,10 @@ import * as React from 'react';
 
 import Button from '@material-ui/core/Button';
 import CardActions from '@material-ui/core/CardActions';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { withStyles } from '@material-ui/core/styles';
+import Switch from '@material-ui/core/Switch'
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import DateRangeIcon from '@material-ui/icons/DateRange';
@@ -20,19 +23,64 @@ const styles = {
 
 };
 
-function Actions(props) {
-	const downloadsPath = "/api/download/" + props.path
-	return (
-		<CardActions {...props}>
-			<Button
-				size="small"
-				variant="text"
-				target="_blank"
-				href={downloadsPath}>
-				<SaveIcon /> &nbsp; Download
-			</Button>
-		</CardActions>
-	)
+class Actions extends React.Component<any, any> {
+ 	public state = {
+		duration: '60',
+		partial: false,
+		start: '0',
+	}
+
+	public render() {
+		const downloadsPath = "/api/download/" + this.props.path
+		const partialDownload = downloadsPath + "?start=" + this.state.start + "&duration=" + this.state.duration
+		const {start, duration, partial} = this.state
+		const style = {visibility: 'hidden'}
+		const startInput = partial ? 
+			<TextField
+				label="Start at"
+				value={start}
+				onChange={this.handleChange('start')}
+			/> : <label style={{color: 'white'}}>Partial</label>
+		const durationInput = partial ?
+			<TextField
+				label="Duration"
+				value={duration}
+				onChange={this.handleChange('duration')}
+			/> : <label style={{color: 'white'}}>Download</label>
+		
+		return (
+			<CardActions {...this.props}>
+				<Button
+					size="small"
+					variant="text"
+					target="_blank"
+					href={partial ? partialDownload : downloadsPath}>
+					<SaveIcon /> &nbsp; Download
+				</Button>
+				<Switch
+					checked={partial}
+					onChange={this.handleSwitchChange('partial')}
+					value="partial"
+					color="primary"
+				/>
+				{ startInput }
+				{ durationInput }
+			</CardActions>
+		)
+	}
+
+	private handleChange = name => event => {
+		this.setState({
+		  [name]: event.target.value,
+		});
+	  };
+	
+	  private handleSwitchChange = name => event => {
+		this.setState({
+		  [name]: event.target.checked,
+		});
+	  };
+	
 }
 
 function Video(props) {
