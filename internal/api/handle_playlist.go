5967b03835c8ffe9c506f1de105752390f73a3e6
Sebastian Himberger <sebastian@himberger.de>
2019-03-09 23:34:56 +0100

    Removed redundant code

--- internal/api/handle_playlist.go
+++ internal/api/handle_playlist.go
@@ -4,13 +4,11 @@ import (
 	"fmt"
 	"github.com/go-chi/chi"
 	"github.com/shimberger/gohls/internal/hls"
-	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func handlePlaylist(w http.ResponseWriter, r *http.Request) {
 	w.Header()["Content-Type"] = []string{hls.PlaylistContentType}
-	log.Infof("URL %v", r.URL)
 	entry := getEntry(r)
 	template := fmt.Sprintf("%v://%v/api/segments/{{.Resolution}}/{{.Segment}}/%v/%v", r.URL.Scheme, r.Host, chi.URLParam(r, "folder"), chi.URLParam(r, "*"))
 	hls.WritePlaylist(template, entry.Path(), w)
