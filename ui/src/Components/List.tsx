1594c56efd2b6b9fd17ff12c258291fddc57aa64
Sebastian Himberger <sebastian@himberger.de>
2021-12-27 18:20:35 +0100

    First refactoring to use hooks

--- ui/src/Components/List.tsx
+++ ui/src/Components/List.tsx
@@ -5,13 +5,16 @@ import { InputAdornment } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { orderBy } from 'lodash';
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import BackButton from '../Presentation/BackButton';
 import Folder from '../Presentation/Folder';
-import ListMessage from '../Presentation/ListMessage';
 import Video from '../Presentation/Video';
-import Page from './Page';
 import { Search as SearchIcon } from "@mui/icons-material"
 import { useParams } from 'react-router';
+import CircularProgress from '@mui/material/CircularProgress';
+import Toolbar from '@mui/material/Toolbar';
+import AppBar from '@mui/material/AppBar';
+import ListMessage from '../Presentation/ListMessage';
 
 const titleStyles = {
 	marginLeft: '24px',
@@ -19,72 +22,59 @@ const titleStyles = {
 	whiteSpace: 'nowrap' as const,
 }
 
-export default function List(props) {
-	const params = useParams()
-	return <List2 params={params} {...props} />
+function Loading() {
+	return (
+		<ListMessage><CircularProgress size={50} /></ListMessage>
+	)
 }
 
-class List2 extends Page<any, any> {
-
-	getInitialState() {
-		return {
-			'folders': null,
-			'name': null,
-			'search': '',
-			'path': null,
-			'parents': null,
-			'videos': null
-		}
-	}
-
-	public fetch(props) {
-		const path = props.params.path || "";
-		this.setState(this.getInitialState())
-		return fetch('/api/item/' + path).then((response) => {
+export default function List(props) {
+	const params = useParams()
+	const [isLoading, setLoading] = useState(true)
+	const [search, setSearch] = useState('')
+	const [data, setData] = useState({
+		'folders': null,
+		'name': null,
+		'path': null,
+		'parents': null,
+		'videos': null
+	})
+	useEffect(() => {
+		setLoading(true)
+		const path = params.path || "";
+		fetch('/api/item/' + path).then((response) => {
 			return response.json()
 		}).then((data) => {
 			return new Promise((resolve) => {
 				let folders = data.children.filter(c => c.type === 'folder')
 				let videos = data.children.filter(c => c.type === 'video')
-				resolve({
+				setData({
 					'folders': orderBy(folders, 'name', 'asc'),
 					'name': data.name,
 					'parents': data.parents,
 					'path': data.path,
 					'videos': orderBy(videos, 'name', 'asc')
 				});
+				setLoading(false)
 			})
 		});
-	}
+	}, [params.path])
 
-	public filter(items) {
-		if (this.state.search) {
-			return items.filter(item => item.name.toLowerCase().indexOf(this.state.search.toLowerCase()) >= 0)
-		}
-		return items
-	}
-
-	onSearch = (e) => {
-		this.setState({
-			search: e.target.value
-		})
-	}
-
-	public toolbar() {
-		const back = (this.state.parents[0]) ? <BackButton to={"/list/" + this.state.parents[0].path} /> : null
+	function toolbar() {
+		const back = (data.parents && data.parents[0]) ? <BackButton to={"/list/" + data.parents[0].path} /> : null
 		return (
 			<React.Fragment>
 				{back}
 				<Typography variant="h6" sx={titleStyles} color="inherit" >
-					{this.state.name}
+					{data.name}
 				</Typography>
 				<Box sx={{ flexGrow: 1 }} />
 				<div>
 					<TextField
 						size="small"
-						value={this.state.search}
+						value={search}
 						placeholder="Search…"
-						onChange={this.onSearch}
+						onChange={onSearch}
 						InputProps={{
 							startAdornment: (
 								< InputAdornment position="start">
@@ -98,11 +88,21 @@ class List2 extends Page<any, any> {
 		)
 	}
 
-	public content() {
+	function filter(items) {
+		if (search) {
+			return items.filter(item => item.name.toLowerCase().indexOf(search.toLowerCase()) >= 0)
+		}
+		return items
+	}
 
-		let folders = this.filter(this.state.folders).map((folder) => <Grid key={folder.name} item={true} xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}><Folder name={folder.name} path={folder.path} /></Grid>)
-		let videos = this.filter(this.state.videos).map((video) => <Grid key={video.name} item={true} xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}><Video name={video.name} info={video.info} path={video.path} /></Grid>)
-		const empty = (this.state.folders != null && (videos.length + folders.length) === 0) ? <ListMessage>No folders or videos found</ListMessage> : null
+	function onSearch(e) {
+		setSearch(e.target.value)
+	}
+
+	function items() {
+		let folders = filter(data.folders).map((folder) => <Grid key={folder.name} item={true} xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}><Folder name={folder.name} path={folder.path} /></Grid>)
+		let videos = filter(data.videos).map((video) => <Grid key={video.name} item={true} xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}><Video name={video.name} info={video.info} path={video.path} /></Grid>)
+		const empty = (data.folders != null && (videos.length + folders.length) === 0) ? <ListMessage>No folders or videos found</ListMessage> : null
 		return (
 			<div style={{ padding: 20, paddingTop: '84px', flexGrow: 1 }}>
 				<Grid spacing={4} container={true} alignItems="stretch">
@@ -114,4 +114,16 @@ class List2 extends Page<any, any> {
 		)
 	}
 
+	return (
+		<div className="page" key="">
+			<AppBar >
+				<Toolbar>
+					{(isLoading) ? null : toolbar()}
+				</Toolbar>
+			</AppBar>
+			<div>
+				{(isLoading) ? <Loading /> : items()}
+			</div>
+		</div>
+	)
 }
